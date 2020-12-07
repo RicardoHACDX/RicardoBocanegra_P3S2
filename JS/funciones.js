9f27a6d5a2019199cf2f711be2792b6dd0c7a022
RicardoBocanegra <ricardomendez145@gmail.com>
2020-12-06 23:00:49 -0600

    funcion

---
+++ JS/funciones.js
@@ -0,0 +1,6 @@
+function abrirPanel (PanelInicial)
+{ const paneles= document.querySelectorAll(".panel")
+paneles ;array.forEach(element =>panel.classList.remove("activa"));
+const opciones = document.querySelectorAll(".opcion")
+opciones.forEach(opcion=>opcion.classList.remove("activo"))
+document.getElementById(panelInicial).classList.add("activa")}
