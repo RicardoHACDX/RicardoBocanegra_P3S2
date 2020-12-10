416032c0b79447d4735176fbb84dc27bf27e2c6a
RicardoBocanegra <ricardomendez145@gmail.com>
2020-12-10 17:29:09 -0600

    p

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,6 +1,42 @@
-function abrirPanel (PanelInicial)
-{ const paneles= document.querySelectorAll(".panel")
-paneles ;array.forEach(element =>panel.classList.remove("activa"));
-const opciones = document.querySelectorAll(".opcion")
-opciones.forEach(opcion=>opcion.classList.remove("activo"))
-document.getElementById(panelInicial).classList.add("activa")}
+function abrirPanel(panelInicial){
+    const paneles = document.querySelectorAll(".panel")
+    paneles.forEach(panel=>panel.classList.remove("activa"));
+    const opciones = document.querySelectorAll(".opcion")
+    opciones.forEach(opcion=>opcion.classList.remove("activo"))
+    document.getElementById(panelInicial).classList.add("activa")
+}
+fetch('../data/bd.json')
+    .then(respuesta=>respuesta.json())
+    .then(datos=>agregarProyectos(datos.proyectos))
+function agregarProyectos(datos){
+    const proyectos = datos
+    const tabla=document.querySelector("#listarProyectos")
+        proyectos.forEach(proyecto=>{
+         tabla.innerHTML+=`
+         
+              ${proyecto.nombre}
+              ${proyecto.lenguaje}
+              ${proyecto.monto}
+         
+         `
+    })
+}
+
+function insertar(evt){
+    evt.preventDefault()
+    
+    const valores=[{
+          nombre:document.getElementById('nombre').value,
+          lenguaje:document.getElementById('lenguaje').value,
+          monto:document.getElementById('monto').value,
+    }]
+
+    agregarProyectos(valores)
+    limpiarFormulario()
+}
+
+function limpiarFormulario(){
+     document.getElementById("nombre").value="";
+     document.getElementById("lenguaje").value="";
+     document.getElementById("monto").value="";
+}
